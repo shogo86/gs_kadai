cc244e78a4e052f679354ed1452c3b1cbaba659c
shogo0329 <show.go.0329@gmail.com>
2016-01-12 20:28:17 +0900

    no message

---
+++ 07_php_database/input_enq.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>アンケート</title>
+</head>
+
+<body>
+    <form method="post" action="confirm_enq.php">
+        ニックネームを入力してください。
+        <br />
+        <input name="name" type="text" style="width:100px">
+        <br /> メールアドレスを入力してください。
+        <br />
+        <input name="email" type="text" style="width:200px">
+        <br /> 年齢を教えてください。
+        <br />
+        <input name="age" type="text" style="width:50px">
+        <br />コメント
+        <br />
+        <input name="comment" type="text" style="width:200px">
+        <br />
+        <input type="submit" value="送信">
+    </form>
+
+</body>
+
+</html>
